7f346df9fca05f4e33d544cb768c47922980e33a
perrydo <perrydo@flemingcollege.ca>
2022-01-25 23:47:29 -0500

    testing

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -13,7 +13,6 @@ function initMap() {
     return new google.maps.Marker({
       position: location,
       label: labels[i % labels.length],
-      fillColor: "blue",
     });
   });
   // Add a marker clusterer to manage the markers.
